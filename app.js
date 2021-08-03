01cf73e43c51e36e584efdafaa86c2201298d5bf
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-08-03 08:11:02 -0500

    morning push

--- app.js
+++ app.js
@@ -6,13 +6,27 @@ const METHOD1 = 'artist.gettoptracks'
 
 const button = document.querySelector('#search')
 
+function renderArtist(music) {
 
+  for (let i = 0; i < 26; i++) {
+    let artistElements = `
+    <h1>${music.Title}</h1>
+    <img src="${music.Poster}">
+    <h2>Year-Released: ${music.Year}</h2>
+    <h3>Type: ${music.Type}</h3>
+
+    `
+  }
+
+
+}
 
 const getArtist = async () => {
   try {
+    //Will be able to retrieve the necessary data from the music api
     const inputArtist = document.querySelector('input').value
     const artist = await axios.get(`${DOMAIN}method=${METHOD1}&artist=${inputArtist}&api_key=${API_KEY}&format=json`)
-    console.log(artist)
+    //console.log(artist.data.toptracks.track)
 
   }
   catch (err) {
